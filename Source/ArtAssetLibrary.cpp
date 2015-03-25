4c9df79a747778d261485e7c0223ad6c9a7b9811
Casey Megginson <tenncasey@gmail.com>
2015-03-25 11:45:23 -0500

    Fixed null pointer issue

--- Source/ArtAssetLibrary.cpp
+++ Source/ArtAssetLibrary.cpp
@@ -5,7 +5,8 @@ ArtAssetLibrary::ArtAssetLibrary() { gDevice = nullptr; }
 
 ArtAssetLibrary::~ArtAssetLibrary() {}
 
-bool ArtAssetLibrary::LoadAssets(GraphicsDevice* gDevice) {
+bool ArtAssetLibrary::LoadAssets(GraphicsDevice* graphics) {
+	gDevice = graphics;
 	library.insert(
 	    std::pair<std::string, Texture*>("Carrier", new Texture()));
 	std::string carr = "./Assets/Images/t_carrier.png";
