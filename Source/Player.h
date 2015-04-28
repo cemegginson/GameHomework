21ada4076356117350b712594f7a298f433e8cf7
Casey Megginson <tenncasey@gmail.com>
2015-04-28 01:44:12 -0500

    Started on componentization of Player class

--- Source/Player.h
+++ Source/Player.h
@@ -1,30 +1,41 @@
-#pragma once
+#include "Component.h"
 
-#include <string>
-#include <list>
-#include "ArtAssetLibrary.h"
-#include "Bullet.h"
-#include "Definitions.h"
-#include "Object.h"
-
-class Player : public Object {
+class Player : public Component {
 protected:
-	ArtAssetLibrary* aLibrary;
-	std::list<Bullet*> bullets;
-	InputDevice* iDevice;
-	uint32 travel;
-	uint32 radius;
-	vector2 center;
-	float32 rotation;
-	float32 lastfiretime;
 
-	b2CircleShape shape; 
-    b2FixtureDef shapefd;
 public:
 	Player();
 	~Player();
-	void Update(float32);
-	void Draw(float32, View*);
-	void Initialize(GraphicsDevice*, Texture*, ArtAssetLibrary*, b2World*, vector2, float32);
-	void setInput(InputDevice*);
-};
+	Update();
+}
+
+// #pragma once
+//
+// #include <string>
+// #include <list>
+// #include "ArtAssetLibrary.h"
+// #include "Bullet.h"
+// #include "Definitions.h"
+// #include "Object.h"
+//
+// class Player : public Object {
+// protected:
+// 	ArtAssetLibrary* aLibrary;
+// 	std::list<Bullet*> bullets;
+// 	InputDevice* iDevice;
+// 	uint32 travel;
+// 	uint32 radius;
+// 	vector2 center;
+// 	float32 rotation;
+// 	float32 lastfiretime;
+//
+// 	b2CircleShape shape;
+//     b2FixtureDef shapefd;
+// public:
+// 	Player();
+// 	~Player();
+// 	void Update(float32);
+// 	void Draw(float32, View*);
+// 	void Initialize(GraphicsDevice*, Texture*, ArtAssetLibrary*, b2World*, vector2, float32);
+// 	void setInput(InputDevice*);
+// };
